ef2f01bc6f4223a547d9e28ec083736c5f5bb06e
Jeff Lu <jefflu235@outlook.com>
2023-10-13 14:03:57 -0400

    new lessons added

--- Unit Two - Using Objects/src/lesson1/CarSimulation.java
+++ Unit Two - Using Objects/src/lesson1/CarSimulation.java
@@ -9,3 +9,5 @@ public class CarSimulation {
         jeep.printCarInfo();
     }
 }
+
+
---
+++ Unit Two - Using Objects/src/lesson1/DogTest.java
@@ -0,0 +1,13 @@
+package lesson1;
+
+public class DogTest {
+    public static void main(String[] args) {
+        Dog d1 = new Dog("Coco", 7, "Poodle");
+        d1.bark();
+        d1.feed();
+        d1.feed();
+
+        boolean cocoIsPuppy = d1.isPuppy();
+        System.out.println(cocoIsPuppy);
+    }
+}
